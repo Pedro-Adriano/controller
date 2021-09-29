d3ceb516e1f17904564ef0b045acb7248a01ec51
Pedro-Adriano <pedrothiagoadriano@gmail.com>
2021-09-29 18:56:14 -0300

    feat: add new urls

--- control/urls.py
+++ control/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from apps.registration.views import home
+from django.views.generic.base import TemplateView
+from apps.registration.views import login_user, submit_login
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("login/", home),
+    path("login/", login_user),
+    path("login/submit", submit_login),
 ]
