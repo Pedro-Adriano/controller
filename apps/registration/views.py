a60fe5b00835e468d8583c807ab7a4c4bbf8cf13
Pedro-Adriano <pedrothiagoadriano@gmail.com>
2021-09-29 18:57:39 -0300

    feat: add authenticate login method

--- apps/registration/views.py
+++ apps/registration/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
-def home(request):
+def login_user(request):
+    print("aquiu1")
     return render(request, "login.html")
+
+
+@csrf_protect
+def submit_login(request):
+    print("aqui")
+    if request.POST:
+        print("aqui")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(username, password)
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("/login/#")
+        else:
+            messages.error(request, "Usuário e senha inválido. Favor tentar novamente")
+    return redirect("/login/")
