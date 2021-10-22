20b22d3d4c6fa7ec8947425ac56969774e6c7b15
Pedro-Adriano <pedrothiagoadriano@gmail.com>
2021-10-22 18:20:17 -0300

    feat: initial commit

--- control/settings.py
+++ control/settings.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 
 from pathlib import Path
 
@@ -28,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "apps",
+    "registration",
 ]
 
 MIDDLEWARE = [
@@ -45,7 +47,7 @@ ROOT_URLCONF = "control.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -66,8 +68,12 @@ WSGI_APPLICATION = "control.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "controller",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
--- control/urls.py
+++ control/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import TemplateView
-from apps.registration.views import login_user, submit_login
+from registration.views import login_user, submit_login, user_register, register_user
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", login_user),
     path("login/submit", submit_login),
+    path("login/register/", register_user),
+    path("login/register/submit", user_register),
 ]
