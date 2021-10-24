4b5d510eddb943b4e5c33d721520d38850f95dd2
Pedro-Adriano <pedrothiagoadriano@gmail.com>
2021-10-23 23:07:54 -0300

    feat: add new url module

--- control/urls.py
+++ control/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import TemplateView
-from registration.views import login_user, submit_login, user_register, register_user
 from django.contrib.auth import views as auth_views
+
+from registration.views import login_user, submit_login, user_register, register_user
+from financial.views import bill_to_pay
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,4 +12,5 @@ urlpatterns = [
     path("login/submit", submit_login),
     path("login/register/", register_user),
     path("login/register/submit", user_register),
+    path("bill_to_pay/", bill_to_pay),
 ]
